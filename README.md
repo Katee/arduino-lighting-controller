5dc8b7f19726a646c8bafeaab1111f53aec75a2f
Katee <hi@kate.io>
2015-05-11 01:17:44 -0400

    Now with modes and buffer over serial support.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Arduino Lighting Controller
 
-This code is the firmware for lighting installation designed and built by (SODIdesigns)[http://sodidesigns.com/]. It uses 7 (wifire16)[http://propaneandelectrons.com/projects/wifire16] boards chained together to control up to 112 lights. The first board in the chain has a (WiFly)[wi://www.sparkfun.com/products/10822] installed.
+This code is the firmware for lighting installation designed and built by [SODIdesigns](http://sodidesigns.com/). It uses 6 [wifire16](http://propaneandelectrons.com/projects/wifire16) boards chained together to control up to 112 lights. The first board in the chain has a [WiFly](http://www.sparkfun.com/products/10822) installed.
 
 By default this firmware displays an animation. It also accepts data from the WiFly and will display that directly. If it hasn't received any valid messages from the WiFly for a while it will go back to showing the default animation.
+
+The data it receives from the WiFly is a simple packet containing 14 bytes of data and  
--- arduino-lighting-controller.ino
+++ arduino-lighting-controller.ino
@@ -8,15 +8,34 @@
 #define DATAPIN  11   // data for 74HC595 shift registers
 #define CLOCKPIN 13   // clock for 74HC595 shift registers
 
-#define bitFlip(x,n)  bitRead(x,n) ? bitClear(x,n) : bitSet(x,n)
 #define STARTUP_TIMEOUT 5000
+#define MESSAGE_WAIT_TIMEOUT 10000
 
-// how long to keep fire active per message, in ms
-#define FIRETIME 100
+#define NUM_BOARDS 6  // the number of boards in the chain
+#define OUTPUT_BYTES_PER_BOARD 2 // it is called the wifire16 :)
 
-char c;
-byte r1 = 0, r2 = 0;
-unsigned long t[16];
+#define PACKET_START 0xee
+#define MESSAGE_BUFFER 0xde
+#define MESSAGE_MODE 0xbe
+
+int numOutputBytes = NUM_BOARDS * OUTPUT_BYTES_PER_BOARD;
+int numChannels = numOutputBytes * 8;
+byte outputBytes[NUM_BOARDS * OUTPUT_BYTES_PER_BOARD] = {0};
+
+int atMessageByte = 0;
+byte readChar;
+byte message[NUM_BOARDS * OUTPUT_BYTES_PER_BOARD] = {0};
+int MESSAGE_LENGTH = numOutputBytes + 2;
+
+unsigned long lastMessageAt = 0;
+unsigned long lastUpdate = 0;
+unsigned long currentStep = 0;
+
+int TIME_BETWEEN_FRAMES = 100;
+
+boolean showPattern = true;
+
+void (*currentPattern)();
 
 void setup() {
   // set all output pins
@@ -28,82 +47,180 @@ void setup() {
   // make sure no lines go active until data is shifted out
   digitalWrite(CLEARPIN, HIGH);
   digitalWrite(OEPIN, LOW);
-
-  // clear any lines that were left active
-  digitalWrite(LATCHPIN, LOW);
-  digitalWrite(OEPIN, HIGH);
-  c = SPI.transfer(0);
-  c = SPI.transfer(0);
-  digitalWrite(LATCHPIN, HIGH);
-  digitalWrite(OEPIN, LOW);
-
+  
   // activate built-in pull-up resistor
   digitalWrite(ARMEDPIN, HIGH);
+
+  setLights();
 
   // ignore the WiFly radio start text
   delay(STARTUP_TIMEOUT);
 
   // start serial communication with the WiFly
   Serial.begin(115200);
-
-  // initialize the expiration time array
-  for(c=0;c<16;c++) {
-    t[c] = 0;
-  }
+  
+  currentPattern = bouncePattern;
 }
 
 void loop() {
-  for(c=0;c<8;c++) {
-    if(t[c] > millis()) {
-      bitSet(r1,c);
+  if (Serial.available() > 0) {
+    readChar = Serial.read();
+    
+    message[atMessageByte] = readChar;
+    atMessageByte++;
+    
+    switch (atMessageByte) {
+      case 1:
+        if (readChar == PACKET_START) {
+          Serial.println("Got message start sigil.");
+        } else {
+          resetStream();
+        }
+        break;
+      case 2:
+        switch (readChar) {
+          case MESSAGE_BUFFER:
+            Serial.println("message is to change pattern");
+            break;
+          case MESSAGE_MODE:
+            Serial.println("message is a buffer");
+            break;
+          default:
+            resetStream();
+            break;
+        }
+      default:
+        if (atMessageByte == MESSAGE_LENGTH) {
+          Serial.println("Got full message.");
+          handleMessage();
+        }
+        break;
     }
-    else {
-      bitClear(r1,c);
+    
+    if (atMessageByte > MESSAGE_LENGTH) {
+      resetStream();
     }
   }
   
-  for(c=0;c<8;c++) {
-    if(t[8+c] > millis()) {
-      bitSet(r2,c);
-    }
-    else {
-      bitClear(r2,c);
+  unsigned long currentMillis = millis();
+  
+  if (!showPattern && (currentMillis - lastMessageAt) > MESSAGE_WAIT_TIMEOUT) {
+    Serial.println("too long since last message, doing pattern");
+    showPattern = true;
+  }
+  
+  if (showPattern) {
+    if ((currentMillis - lastUpdate) > TIME_BETWEEN_FRAMES) {
+      setLights();
+      currentPattern();
+      
+      lastUpdate = currentMillis;
+      currentStep++;
     }
   }
+}
 
-  digitalWrite(LATCHPIN, LOW);
-  digitalWrite(OEPIN, HIGH);
-  c = SPI.transfer(r2);
-  c = SPI.transfer(r1);
-  digitalWrite(LATCHPIN, HIGH);
-  digitalWrite(OEPIN, LOW);
+void resetStream() {
+  Serial.println("resetting stream");
+  atMessageByte = 0;
+}
 
-  if(Serial.available() > 1) {
-    c = Serial.read();
-    if( c == 0x01 ) {
-      c = Serial.read();
-      switch(c) {
-        case '0' : t[0] = millis() + FIRETIME; break;
-        case '1' : t[1] = millis() + FIRETIME; break;
-        case '2' : t[2] = millis() + FIRETIME; break;
-        case '3' : t[3] = millis() + FIRETIME; break;
-        case '4' : t[4] = millis() + FIRETIME; break;
-        case '5' : t[5] = millis() + FIRETIME; break;
-        case '6' : t[6] = millis() + FIRETIME; break;
-        case '7' : t[7] = millis() + FIRETIME; break;
-        case '8' : t[8] = millis() + FIRETIME; break;
-        case '9' : t[9] = millis() + FIRETIME; break;
-        case 'a' : case 'A' : t[10] = millis() + FIRETIME; break;
-        case 'b' : case 'B' : t[11] = millis() + FIRETIME; break;
-        case 'c' : case 'C' : t[12] = millis() + FIRETIME; break;
-        case 'd' : case 'D' : t[13] = millis() + FIRETIME; break;
-        case 'e' : case 'E' : t[14] = millis() + FIRETIME; break;
-        case 'f' : case 'F' : t[15] = millis() + FIRETIME; break;
-        case '?' :
-          // load power on = low, off = high
-          digitalRead(ARMEDPIN) ? Serial.print("-") : Serial.print("+");
+void handleMessage() {
+  switch (message[1]) {
+    case MESSAGE_MODE:
+      Serial.println(message[2]);
+      
+      switch (message[2]) {
+        case 0x01:
+          currentPattern = chasePattern;
+          break;
+        case 0x02:
+          currentPattern = bouncePattern;
           break;
       }
-    }
+      
+      showPattern = true;
+      break;
+    case MESSAGE_BUFFER:
+      Serial.println("showing buffer");
+      
+      lastMessageAt = millis();
+      showPattern = false;
+      
+      for (int i = 0; i < numOutputBytes; i++) {
+        outputBytes[i] = message[i + 2];
+      }
+      setLights();
+      break;
+  }
+  
+  resetStream();
+}
+
+void chasePattern() {
+  if (currentStep >= numChannels * 2) {
+    currentStep = 0;
+  }
+  
+  // Clear all lights
+  for (int channel = 0; channel < numChannels; channel++) {
+    bitSet(outputBytes[channel / 8], channel % 8);
+  }
+  
+  int centerLight;
+  
+  if (currentStep < numChannels) {
+    centerLight = currentStep;
+  } else if (currentStep < (numChannels * 2)) {
+    centerLight = (numChannels * 2) - currentStep;
+  }
+  
+  channelClear(centerLight - 1);
+  channelClear(centerLight);
+  channelClear(centerLight + 1);
+}
+
+void bouncePattern() {
+  if (currentStep >= (numChannels - 2) - 1) {
+    currentStep = 0;
+  }
+  
+  // Clear all lights
+  for (int channel = 0; channel < numChannels; channel++) {
+    bitSet(outputBytes[channel / 8], channel % 8);
+  }
+  
+  int topLight = currentStep;
+  int bottomLight = numChannels - currentStep;
+  
+  channelClear(topLight);
+  channelClear(topLight + 1);
+  
+  channelClear(bottomLight - 1);
+  channelClear(bottomLight - 2);
+}
+
+void setLights() {
+  digitalWrite(LATCHPIN, LOW);
+  digitalWrite(OEPIN, HIGH);
+  
+  // actually send out the buffer
+  for (int i = 0; i < numOutputBytes; i++) {
+    SPI.transfer(outputBytes[i]);
+  }
+  
+  digitalWrite(LATCHPIN, HIGH);
+  digitalWrite(OEPIN, LOW);
+}
+
+void channelSet(int channel) {
+  if (channel >= 0 && channel < numChannels) {
+    bitSet(outputBytes[channel / 8], channel % 8);
+  }
+}
+
+void channelClear(int channel) {
+  if (channel >= 0 && channel < numChannels) {
+    bitClear(outputBytes[channel / 8], channel % 8);
   }
 }
